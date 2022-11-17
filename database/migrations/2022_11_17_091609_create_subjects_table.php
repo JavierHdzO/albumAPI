04a752d1adf40e88a5b21a7bbdbfb7553a3cae90
Rub Lara <rubiselreyeslara.rrl@gmail.com>
2022-11-17 03:29:45 -0600

    Bases de datos, migraciones basicas

--- database/migrations/2022_11_17_091609_create_subjects_table.php
+++ database/migrations/2022_11_17_091609_create_subjects_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('subjects', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
+            $table->integer('grade');
+            $table->string('group');
+            $table->string('profesor');
+            $table->date('begin_schedule');
+            $table->date('end_schedule');
+            $table->integer('album_id');
         });
     }
 
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('subjects');
     }
 };
